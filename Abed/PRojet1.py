5afdd27a0f8a81bb18b97c3bad8d4da984529f13
Abed715 <206304632@cegepsherbrooke.qc.ca>
2026-02-18 09:53:51 -0500

    fin des if

--- Abed/PRojet1.py
+++ Abed/PRojet1.py
@@ -51,12 +51,27 @@ def fonction_arbre():
             if choix3 == "1":
 
                 choix4 = poser_question(
-                    "Bobby entre dans la cabanne et le bruit effrayant est un bucheron couvert de sang qui decoupe une viande rouge",
-                    "",
-                    ""
-
-
+                    "Bobby entre dans la cabanne et le bruit effrayant est un bucheron couvert de sang qui decoupe une viande rouge a l'aide d'une tronsoneuse",
+                    "à cause du bruit de la tronsonneuse le bucheron ne le remarque pas et bobby commence a parler au bucheron",
+                    "à cause du bruit de la tronsonneuse le bucheron ne le remarque pas et bobby profite de l'ocasion pour assomer le bucheron par derrière"
                 )
+
+                if choix4 == "1":
+
+                    choix5 = poser_question(
+                        "le bucheron se retourne brusquement vers Bobby",
+                        "Bobby le regarde et lui demande qu'est-ce qu'il fait avec la viande et la tronsonneuse",
+                        "bobby lui demande est-ce qu'il peut se refugier dans sa cabanne jusqu'à ce qu'il peut partir"
+                    )
+
+                    if choix5 == "1":
+                        print("Fin : LE bucheron tue Bobby parce que la viande étaitun humain. Il était un tueur en serie en cavale.")
+                    
+                    elif choix5 == "2":
+                        print("Fin : Bobby reste un peu et pendant que le bucheron dormait il s'enfuit pendant la nuit en volant des chose utile a sa survie.")
+                
+
+                elif choix4 == "2":
             
             elif choix3 == "2":
 
